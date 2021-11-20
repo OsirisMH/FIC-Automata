3080b1c38d00b5460cd7d43cabe64cd797233990
Ruben <skyline7514@gmail.com>
2021-11-20 09:48:23 -0700

    Version 2 del automata bidireccional

--- automataTWA.py
+++ automataTWA.py
@@ -1,48 +1,48 @@
 import json 
-data = open('Config.json')
+data = open('ConfiguracionTWA.json')
 config = json.load(data) 
-with open("Cadena.txt", 'r') as f:
+with open("CadenaTWA.txt", 'r') as f:
     datosEntrada = f.read()
-    
+
 parametros = {}
 parametros['estados'] = config['estados']
 parametros['alfabeto'] = config['alfabeto']
-for i in config['transiciones']:    
+for i in config['transiciones']:
     parametros['transiciones'] = i
 parametros['estadoInicial'] = config['estadoInicial']
-parametros['direccionInicial'] = config['direccionInicial']
-parametros['direccionAceptacion'] = config['direccionAceptacion']
 parametros['estadoAceptacion'] = config['estadoAceptacion']
 
-estadoActual = parametros['estadoInicial'] 
-direccionAnterior = parametros['direccionInicial']
+estadoActual = parametros['estadoInicial']
+direccion=""
 posicionCaracter = 0
+vueltas=0
+contador=len(parametros['alfabeto'])-1
+longitudDatosEntrada = len(datosEntrada)-1
 
-if(parametros['direccionInicial'] == 'L'):
-    datosEntrada = datosEntrada[::-1]
+while vueltas <(longitudDatosEntrada*2)-1:
     
-print("CADENA INICIAL: " + datosEntrada)
-print("ESTADO INCIAL: " +  estadoActual + "\n")
-
-for i in datosEntrada:
     direccion = parametros['transiciones'][estadoActual][str(datosEntrada[posicionCaracter])][1]
-    if(direccion != direccionAnterior):
-        datosEntrada = datosEntrada[::-1]
-    direccionAnterior = direccion
     estadoActual = parametros['transiciones'][estadoActual][str(datosEntrada[posicionCaracter])][0]
-     
-    print("Posicion Caracter: " + str(posicionCaracter))   
+    
+    if(direccion == parametros['alfabeto'][contador]):
+        posicionCaracter = posicionCaracter-1
+
+    print("Posicion Caracter: " + str(posicionCaracter))
+    print("Vuelta: ",vueltas)
     print("Direccion: " + direccion)
     print("Cadena: " + datosEntrada)
     print("Caracter: " + datosEntrada[posicionCaracter])
     print("Estado: " + estadoActual + "\n")
     
-    posicionCaracter +=1 
-    if(posicionCaracter > len(datosEntrada)):
-        break
-        
-print("Posicion: " + estadoActual)
-if((estadoActual in parametros['estadoAceptacion']) and (direccion in parametros['direccionAceptacion'])):
+    if(direccion == parametros['alfabeto'][contador-1]):
+        posicionCaracter +=1
+
+    vueltas+=1
+  
+
+print("Vueltas totales ",vueltas)
+print("Longitud de la cadena ",len(datosEntrada))
+if((estadoActual in parametros['estadoAceptacion']) and (vueltas > longitudDatosEntrada)):
     print("Aceptado") 
 else:
     print("Rechazado")
